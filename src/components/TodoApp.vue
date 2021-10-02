88b792306579ee9b7d4baf5d1eadf990fd096677
dunekdev <losingfireflies23@gmail.com>
2021-10-02 09:07:09 +0200

    init

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <input type="text" placeholder="todo" v-model="newItem">
+    <button @click="addItem">dodaj</button>
+    <TodoItem v-for="item in items" :key="item.id" :item="item" :style="style" @removeClicked="completeItem" @deleteClicked="removeItem" />
+    
+  </div>
+</template>
+
+<script>
+
+import TodoItem from './TodoItem.vue'
+
+export default {
+    components: {
+        TodoItem,
+    },
+  data(){ 
+    return{
+        style: {
+            color: '#000',
+        },
+      newItem: '',
+      items: []
+    }
+  },
+  methods: {
+    addItem(){
+      this.items.push({title: this.newItem, completed: false, id: Math.random()})
+      this.newItem = ''
+    },
+    removeItem(id){
+      const index = this.items.findIndex(el => el.id === id)
+      this.items.splice(index,1);
+    },
+    completeItem(id){
+      const index = this.items.findIndex(el => el.id === id)
+      this.items[index].completed = true
+    }
+  }
+}
+</script>
+
+<style>
+  .item {
+    border: 1px solid #cdcdcd;
+    margin: 10px;
+    padding: 10px;
+  }
+
+    .completed {
+    opacity: 0.5;
+
+    }
+
+  .completed h2 {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="item" :class="{completed: item.completed}">
+    <h2>{{ item.title }}</h2>
+    <button v-if="!item.completed" @click="completeItem()">Ukończone</button><button @click="removeItem()">Usuń</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['item'],
+    methods: {
+        completeItem(){
+            this.$emit('removeClicked', this.item.id)
+        },
+        removeItem(){
+            this.$emit('deleteClicked', this.item.id)
+        }
+    }
+}
+</script>
